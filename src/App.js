473f8005bbf703e389bee008cb02a9b20977d99d
tech-911 <babatundeolatunji74@gmail.com>
2023-04-19 20:25:50 +0100

    ...updating

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           appId={process.env.REACT_APP_FACEBOOK_APP_ID}
           fields="name,email,picture"
           callback={responseFacebook}
-          cssClass="my-custom-class"
+          cssClass="my-custom-clas"
         />
       </div>
     </div>
