d7d8db2c7a8ad1e376aa50d0d4839c348ad5e166
tech-911 <babatundeolatunji74@gmail.com>
2023-04-23 10:09:16 +0100

    ...updating

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import "./App.scss";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import FacebookLogin from "react-facebook-login";
-import GoogleLogin from "react-google-login";
+import jwt_decode from "jwt-decode";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 
-function App() {
-  const value = useSelector((state) => state);
-  console.log(value);
+const App = () => {
+  // const value = useSelector((state) => state);
+  // console.log(value);
   const responseFacebook = (response) => {
-    console.log(response);
+    console.log("FacebookUser: ", response);
   };
 
   return (
     <div className="App">
-      <div className="facebookstyle">
-        <FacebookLogin
-          appId={process.env.REACT_APP_FACEBOOK_APP_ID}
-          fields="name,email,picture"
-          callback={responseFacebook}
-          cssClass="my-custom-clas"
+      <FacebookLogin
+        appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        cssClass="my-custom-clas"
+      />
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_APP_ID}>
+      <div className="tunde">
+        <GoogleLogin
+          className={"checking"}
+          onSuccess={(credentialResponse) => {
+            const details = jwt_decode(credentialResponse.credential);
+            console.log("details: ", details);
+            console.log("credentialResponse: ", credentialResponse);
+          }}
+          theme="filled_black"
+          shape="pill"
+          onError={() => {
+            console.log("Login Failed");
+          }}
         />
       </div>
+        
+      </GoogleOAuthProvider>
     </div>
   );
-}
+};
 
 export default App;
