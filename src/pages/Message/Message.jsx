ecfee0c18f98b9cb171e2005baedaafb6013d15f
Aliyu Nurudeen <deen.aliyu40@gmail.com>
2023-07-09 21:29:34 +0100

    new

---
+++ src/pages/Message/Message.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Empty2 from "../../assets/png/msg1.png";
+const Message = () => {
+
+  return (
+
+   <>
+   <div className="-mt-[75px] -ml-[300px]">
+   <img src={Empty2} alt="" className="w-[2400px]"/>
+   </div>
+   </>
+  )
+}
+
+export default Message
